24ccaa81143da8be40ccffec125265fefe4b2e5f
Udit Sharma <101125913+Uditxarma@users.noreply.github.com>
2024-06-21 17:14:09 +0530

    Add files via upload

---
+++ Array/reverse_array.py
@@ -0,0 +1,5 @@
+"""Write a Python program to reverse the order of the items in the array."""
+from array import *
+array_one =array('i', [1, 3, 5, 3, 7, 1, 9, 3])
+array_one.reverse()
+print(array_one)
